85b885ad137ed5fde493305f2b1cd895a053819d
beagrie <38009915+beagrie@users.noreply.github.com>
2018-04-02 16:28:54 +0100

    Add files via upload

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour {
+
+	public MeshRenderer meshRenderer;
+	public MeshFilter meshFilter;
+
+	void Start () {
+
+		int vertexIndex = 0;
+		List<Vector3> vertices = new List<Vector3> ();
+		List<int> triangles = new List<int> ();
+		List<Vector2> uvs = new List<Vector2> ();
+
+		for (int p = 0; p < 6; p++) { 
+			for (int i = 0; i < 6; i++) {
+
+				int triangleIndex = VoxelData.voxelTris [p, i];
+				vertices.Add (VoxelData.voxelVerts [triangleIndex]);
+				triangles.Add (vertexIndex);
+
+				uvs.Add (VoxelData.voxelUvs [i]);
+
+				vertexIndex++;
+
+			}
+		}
+
+		Mesh mesh = new Mesh ();
+		mesh.vertices = vertices.ToArray ();
+		mesh.triangles = triangles.ToArray ();
+		mesh.uv = uvs.ToArray ();
+
+		mesh.RecalculateNormals ();
+
+		meshFilter.mesh = mesh;
+		
+	}
+
+}
---
+++ Assets/Scripts/VoxelData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VoxelData {
+
+
+	public static readonly Vector3[] voxelVerts = new Vector3[8] {
+
+		new Vector3(0.0f, 0.0f, 0.0f),
+		new Vector3(1.0f, 0.0f, 0.0f),
+		new Vector3(1.0f, 1.0f, 0.0f),
+		new Vector3(0.0f, 1.0f, 0.0f),
+		new Vector3(0.0f, 0.0f, 1.0f),
+		new Vector3(1.0f, 0.0f, 1.0f),
+		new Vector3(1.0f, 1.0f, 1.0f),
+		new Vector3(0.0f, 1.0f, 1.0f),
+
+	};
+
+	public static readonly int[,] voxelTris = new int[6,6] {
+
+		{0, 3, 1, 1, 3, 2}, // Back Face
+		{5, 6, 4, 4, 6, 7}, // Front Face
+		{3, 7, 2, 2, 7, 6}, // Top Face
+		{1, 5, 0, 0, 5, 4}, // Bottom Face
+		{4, 7, 0, 0, 7, 3}, // Left Face
+		{1, 2, 5, 5, 2, 6} // Right Face
+
+	};
+
+	public static readonly Vector2[] voxelUvs = new Vector2[6] {
+
+		new Vector2 (0.0f, 0.0f),
+		new Vector2 (0.0f, 1.0f),
+		new Vector2 (1.0f, 0.0f),
+		new Vector2 (1.0f, 0.0f),
+		new Vector2 (0.0f, 1.0f),
+		new Vector2 (1.0f, 1.0f)
+
+	};
+
+
+}
