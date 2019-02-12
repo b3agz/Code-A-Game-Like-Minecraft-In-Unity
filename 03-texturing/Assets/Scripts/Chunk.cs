9d00d04c0e654980e3de662ca86ffbe773ce3813
beagrie <38009915+beagrie@users.noreply.github.com>
2019-02-12 19:31:50 +0000

    Asset files for part three of Minecraft in Unity tutorial series.

---
+++ 03-texturing/Assets/Scripts/Chunk.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour {
+
+	public MeshRenderer meshRenderer;
+	public MeshFilter meshFilter;
+
+	int vertexIndex = 0;
+	List<Vector3> vertices = new List<Vector3> ();
+	List<int> triangles = new List<int> ();
+	List<Vector2> uvs = new List<Vector2> ();
+
+	byte[,,] voxelMap = new byte[VoxelData.ChunkWidth, VoxelData.ChunkHeight, VoxelData.ChunkWidth];
+
+    World world;
+
+	void Start () {
+
+        world = GameObject.Find("World").GetComponent<World>();
+
+		PopulateVoxelMap ();
+		CreateMeshData ();
+		CreateMesh ();
+
+	}
+
+	void PopulateVoxelMap () {
+		
+		for (int y = 0; y < VoxelData.ChunkHeight; y++) {
+			for (int x = 0; x < VoxelData.ChunkWidth; x++) {
+				for (int z = 0; z < VoxelData.ChunkWidth; z++) {
+
+                    if (y < 1)
+                        voxelMap[x, y, z] = 0;
+                    else if (y == VoxelData.ChunkHeight - 1)
+                        voxelMap[x, y, z] = 3;
+                    else
+                        voxelMap [x, y, z] = 1;
+    
+				}
+			}
+		}
+
+	}
+
+	void CreateMeshData () {
+
+		for (int y = 0; y < VoxelData.ChunkHeight; y++) {
+			for (int x = 0; x < VoxelData.ChunkWidth; x++) {
+				for (int z = 0; z < VoxelData.ChunkWidth; z++) {
+
+					AddVoxelDataToChunk (new Vector3(x, y, z));
+
+				}
+			}
+		}
+
+	}
+
+	bool CheckVoxel (Vector3 pos) {
+
+		int x = Mathf.FloorToInt (pos.x);
+		int y = Mathf.FloorToInt (pos.y);
+		int z = Mathf.FloorToInt (pos.z);
+
+		if (x < 0 || x > VoxelData.ChunkWidth - 1 || y < 0 || y > VoxelData.ChunkHeight - 1 || z < 0 || z > VoxelData.ChunkWidth - 1)
+			return false;
+
+		return world.blocktypes[voxelMap [x, y, z]].isSolid;
+
+	}
+
+	void AddVoxelDataToChunk (Vector3 pos) {
+
+		for (int p = 0; p < 6; p++) { 
+
+			if (!CheckVoxel(pos + VoxelData.faceChecks[p])) {
+
+                byte blockID = voxelMap[(int)pos.x, (int)pos.y, (int)pos.z];
+
+				vertices.Add (pos + VoxelData.voxelVerts [VoxelData.voxelTris [p, 0]]);
+				vertices.Add (pos + VoxelData.voxelVerts [VoxelData.voxelTris [p, 1]]);
+				vertices.Add (pos + VoxelData.voxelVerts [VoxelData.voxelTris [p, 2]]);
+				vertices.Add (pos + VoxelData.voxelVerts [VoxelData.voxelTris [p, 3]]);
+
+                AddTexture(world.blocktypes[blockID].GetTextureID(p));
+
+				triangles.Add (vertexIndex);
+				triangles.Add (vertexIndex + 1);
+				triangles.Add (vertexIndex + 2);
+				triangles.Add (vertexIndex + 2);
+				triangles.Add (vertexIndex + 1);
+				triangles.Add (vertexIndex + 3);
+				vertexIndex += 4;
+
+			}
+		}
+
+	}
+
+	void CreateMesh () {
+
+		Mesh mesh = new Mesh ();
+		mesh.vertices = vertices.ToArray ();
+		mesh.triangles = triangles.ToArray ();
+		mesh.uv = uvs.ToArray ();
+
+		mesh.RecalculateNormals ();
+
+		meshFilter.mesh = mesh;
+
+	}
+
+    void AddTexture (int textureID) {
+
+        float y = textureID / VoxelData.TextureAtlasSizeInBlocks;
+        float x = textureID - (y * VoxelData.TextureAtlasSizeInBlocks);
+
+        x *= VoxelData.NormalizedBlockTextureSize;
+        y *= VoxelData.NormalizedBlockTextureSize;
+
+        y = 1f - y - VoxelData.NormalizedBlockTextureSize;
+
+        uvs.Add(new Vector2(x, y));
+        uvs.Add(new Vector2(x, y + VoxelData.NormalizedBlockTextureSize));
+        uvs.Add(new Vector2(x + VoxelData.NormalizedBlockTextureSize, y));
+        uvs.Add(new Vector2(x + VoxelData.NormalizedBlockTextureSize, y + VoxelData.NormalizedBlockTextureSize));
+
+
+    }
+
+}
---
+++ 03-texturing/Assets/Scripts/World.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+
+    public Material material;
+    public BlockType[] blocktypes;
+
+}
+
+[System.Serializable]
+public class BlockType {
+
+    public string blockName;
+    public bool isSolid;
+
+    [Header("Texture Values")]
+    public int backFaceTexture;
+    public int frontFaceTexture;
+    public int topFaceTexture;
+    public int bottomFaceTexture;
+    public int leftFaceTexture;
+    public int rightFaceTexture;
+
+    // Back, Front, Top, Bottom, Left, Right
+
+    public int GetTextureID (int faceIndex) {
+
+        switch (faceIndex) {
+
+            case 0:
+                return backFaceTexture;
+            case 1:
+                return frontFaceTexture;
+            case 2:
+                return topFaceTexture;
+            case 3:
+                return bottomFaceTexture;
+            case 4:
+                return leftFaceTexture;
+            case 5:
+                return rightFaceTexture;
+            default:
+                Debug.Log("Error in GetTextureID; invalid face index");
+                return 0;
+
+
+        }
+
+    }
+
+}
