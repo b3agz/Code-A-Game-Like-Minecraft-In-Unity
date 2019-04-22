380803b957588ef4ec3960c195e52d7ada539aa7
beagrie <38009915+beagrie@users.noreply.github.com>
2019-04-22 19:56:43 +0100

    Assets files for part nine fo Code a Game Like Minecraft in Unity tutorial.

---
+++ 09-scrollable-toolbelt/Assets/Scripts/Noise.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise  {
+
+    public static float Get2DPerlin (Vector2 position, float offset, float scale) {
+
+        return Mathf.PerlinNoise((position.x + 0.1f) / VoxelData.ChunkWidth * scale + offset, (position.y + 0.1f) / VoxelData.ChunkWidth * scale + offset);
+
+    }
+
+    public static bool Get3DPerlin (Vector3 position, float offset, float scale, float threshold) {
+
+        // https://www.youtube.com/watch?v=Aga0TBJkchM Carpilot on YouTube
+
+        float x = (position.x + offset + 0.1f) * scale;
+        float y = (position.y + offset + 0.1f) * scale;
+        float z = (position.z + offset + 0.1f) * scale;
+
+        float AB = Mathf.PerlinNoise(x, y);
+        float BC = Mathf.PerlinNoise(y, z);
+        float AC = Mathf.PerlinNoise(x, z);
+        float BA = Mathf.PerlinNoise(y, x);
+        float CB = Mathf.PerlinNoise(z, y);
+        float CA = Mathf.PerlinNoise(z, x);
+
+        if ((AB + BC + AC + BA + CB + CA) / 6f > threshold)
+            return true;
+        else
+            return false;
+
+    }
+
+}
